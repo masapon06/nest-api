eacd0da7051fb9c50f935992f6de5eae5b331643
masapon0618 <masapon0618@gmail.com>
2021-09-22 00:48:26 +0900

    fix モックが作成されない不具合修正

--- src/bars/__mocks__/bars.service.ts
+++ src/bars/__mocks__/bars.service.ts
@@ -1,5 +1,5 @@
 import { barStub } from '../test/stubs/bars.stub'
 
 export const BarsService = jest.fn().mockReturnValue({
-  findAll: jest.fn().mockResolvedValue([barStub()]),
+  find: jest.fn().mockResolvedValue([barStub()]),
 })
